82b16d7ee9171ba51fdb7c2bfc9cfbcb884951a4
MarcWaw <marcin.pawel.wawszczak@gmail.com>
2021-05-07 16:54:15 +0200

    GitCommit

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.ensemble import BaggingClassifier
+from sklearn import model_selection
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data = pd.read_csv('Data\cumulative.csv')
+X = data.drop(columns=['kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_score',
+                       'koi_fpflag_nt', 'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec'])
+# Brak w bazie: 'koi_vet_stat', 'koi_vet_date', 'koi_disp_prov', 'koi_comment'
 
+y = data['koi_disposition']
+for i in range(len(y)):
+    if y[i] == 'CONFIRMED':
+        y.iloc[i] = 1
+    else:
+        y.iloc[i] = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+kf = KFold(n_splits=10, shuffle=True, random_state=1410)
+scores = []
 
+# for train_index, test_index in kf.split(X):
+#     X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+#     y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+base_cls = DecisionTreeClassifier()
+clf = BaggingClassifier(base_estimator=base_cls, n_estimators=10, random_state=0)
+results = model_selection.cross_val_score(clf, X, y, cv = kf)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("accuracy :")
+print(results.mean())
