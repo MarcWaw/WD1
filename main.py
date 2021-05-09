d4abfb7d72cc840d59f636c9e54c743ccc60ec59
MarcWaw <marcin.pawel.wawszczak@gmail.com>
2021-05-09 22:47:01 +0200

    Działa - dodanie metryk i modyfikacja zbioru

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import numpy as np
 import tqdm
+import matplotlib.pyplot as plt
+import RadarChart
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
@@ -12,10 +13,11 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
 from imblearn.over_sampling import SMOTE
 
-data = pd.read_csv('Data\cumulative.csv')
-X = data.drop(
-    columns=['kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_score', 'koi_fpflag_nt',
-             'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec', 'koi_tce_delivname', 'koi_teq_err1', 'koi_teq_err2'])
+data = pd.read_csv('Data\ezgo.csv')
+X = data.drop(columns=['koi_disposition'])
+# .drop(
+# columns=['rowid', 'kepid', 'kepoi_name', 'kepler_name', 'koi_disposition', 'koi_pdisposition', 'koi_score', 'koi_fpflag_nt',
+#          'koi_fpflag_ss', 'koi_fpflag_co', 'koi_fpflag_ec', 'koi_tce_delivname', 'koi_teq_err1', 'koi_teq_err2'])
 # Brak w bazie: 'koi_vet_stat', 'koi_vet_date', 'koi_disp_prov', 'koi_comment'
 # Dodatkowo: koi_tce_delivname (jakaś nazwa); koi_teq_err1, koi_teq_err2 (puste)
 
@@ -32,7 +34,7 @@ y = pd.DataFrame(y_list, columns=['koi_disposition'])
 
 # LogisticRegression() - wywala błędy póki co, może trzeba standaryzacje zrobić
 estimators = [DecisionTreeClassifier(), SVC(), KNeighborsClassifier(), GaussianNB(),
-              LogisticRegression(solver='lbfgs', max_iter=1000)]
+              LogisticRegression(solver='lbfgs', max_iter=100000)]
 names = ['Drzewa Decyzyjne', 'SVM', 'kNN', 'Naiwny Bayes', 'Regresja logistyczna']
 result = [0 for i in range(len(estimators))]
 
@@ -44,7 +46,8 @@ sm = SMOTE(random_state=1410)
 X_resampled, y_resampled = sm.fit_resample(X, y)
 min_max_scaler = preprocessing.MinMaxScaler()
 
-times_cross_validation = 10
+# Krotność walidacji krzyżowej
+times_cross_validation = 5
 
 # Walidacja krzyżowa
 kf = KFold(n_splits=times_cross_validation, shuffle=True, random_state=1410)
@@ -52,12 +55,6 @@ kf = KFold(n_splits=times_cross_validation, shuffle=True, random_state=1410)
 for train_index, test_index in tqdm.tqdm(kf.split(X_resampled)):
     X_train, X_test = X_resampled.iloc[train_index], X_resampled.iloc[test_index]
     y_train, y_test = y_resampled.iloc[train_index], y_resampled.iloc[test_index]
-
-    # Wykonanie predykcji dla algorytmu Drzew, który nie wymaga skalowania danych
-    clf = BaggingClassifier(base_estimator=estimators[0], n_estimators=10, random_state=0).fit(X_train,
-                                                                                               y_train.values.ravel())
-    predict = clf.predict(X_test)
-    result[0] += accuracy_score(y_test, predict)
 
     # Skalowanie danych, żeby wszytskie wartości znajdowały się w zakresie (0, 1)
     X_train = min_max_scaler.fit_transform(X_train)
@@ -67,8 +64,49 @@ for train_index, test_index in tqdm.tqdm(kf.split(X_resampled)):
     for i in range(1, len(estimators)):
         clf = BaggingClassifier(base_estimator=estimators[i], n_estimators=10, random_state=0).fit(
             X_train, y_train.values.ravel())
-        predict = clf.predict(X_test)
-        result[i] += accuracy_score(y_test, predict)
+        prediction = clf.predict(X_test)
+        result_accuracy[i] += accuracy_score(y_test, prediction)
+        result_precision[i] += precision_score(y_test, prediction)
+        result_recall[i] += recall_score(y_test, prediction)
+        result_f1[i] += f1_score(y_test, prediction)
 
-for i in range(len(estimators)):
-    print(f'{names[i]} {result[i] / times_cross_validation}')
+    X_train = min_max_scaler.fit_transform(X_train)
+    X_test = min_max_scaler.fit_transform(X_test)
+    clf = BaggingClassifier(base_estimator=estimators[-1], n_estimators=10, random_state=0).fit(
+        X_train, y_train.values.ravel())
+    prediction = clf.predict(X_test)
+    result_accuracy[-1] += accuracy_score(y_test, prediction)
+    result_precision[-1] += precision_score(y_test, prediction)
+    result_recall[-1] += recall_score(y_test, prediction)
+    result_f1[-1] += f1_score(y_test, prediction)
+
+# Wyświetlanie wyników
+metric_names = ['Accuracy', 'Precision', 'Recall', 'F1']
+# N = len(metric_names)
+# theta = RadarChart.radar_factory(N, frame='polygon')
+
+j = 0
+for metric in [result_accuracy, result_precision, result_recall, result_f1]:
+    print('\nMetryka: ' + metric_names[j])
+    for i in range(len(estimators)):
+        print(f'{names[i]} {metric[i] / times_cross_validation}')
+    j += 1
+
+# fig, axs = plt.subplots(subplot_kw=dict(projection='radar'))
+# colors = ['b', 'r', 'g', 'm', 'y']
+
+# Plot the four cases from the example data on separate axes
+# for ax, (title, case_data) in zip(axs.flat, data):
+#     ax.set_rgrids([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+#     ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
+#                  horizontalalignment='center', verticalalignment='center')
+#     for d, color in zip(case_data, colors):
+#         ax.plot(theta, d, color=color)
+#         ax.fill(theta, d, facecolor=color, alpha=0.25)
+#     ax.set_varlabels(spoke_labels)
+
+# add legend relative to top-left plot
+# labels = ('Accuracy', 'Precision', 'Recall', 'F1')
+# legend = axs[0, 0].legend(labels, loc=(0.9, .95), labelspacing=0.1, fontsize='small')
+# fig.text(0.5, 0.965, 'Metryki', horizontalalignment='center', color='black', weight='bold', size='large')
+# plt.show()
